a509b61fd4941f56ec229e1ca0f81be68566b1c7
Alexander Karbstein <alexander.karbstein@gmail.com>
2017-06-25 21:33:19 +0200

    Added failing test to check CI toolchain

--- tests/test_peg_solitaire.py
+++ tests/test_peg_solitaire.py
@@ -1,7 +1,20 @@
 import unittest
 import peg_solitaire
 
+
 class TestPegSolitaire(unittest.TestCase):
     def test_default_game(self):
         game = peg_solitaire.Game()
-        self.assertIsNotNone(game,msg="Game was not successfully initialized")
+        self.assertIsNotNone(game, msg="Game was not successfully initialized")
+        # After initialization the playing field should be initialized like that:
+        x = "x"
+        playing_field = [
+            [x, x, 1, 1, 1, x, x],
+            [x, x, 1, 1, 1, x, x],
+            [1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 0, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1],
+            [x, x, 1, 1, 1, x, x],
+            [x, x, 1, 1, 1, x, x],
+        ]
+        self.assertEqual(game.playing_field(),playing_field,msg="Playing field not correctly initialized")
